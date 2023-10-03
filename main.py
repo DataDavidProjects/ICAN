f56966e98c2c3f9d7b9e4b1ffa1d0f97c13506ae
Davide Lupis <davide_lupis@yahoo.it>
2023-10-03 20:08:45 +0200

    HR feature to suggest most qualified agents

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import os
 import yaml
 from dotenv import load_dotenv
-from src.interaction.conversation import (
-    Interaction,
-)
-from src.utils.agent_utils import (
-    create_agents,
-)
+from src.interaction.conversation import Interaction
+from src.utils.agent_utils import create_agents, suggest_agents
 
 # Load environment variables
 load_dotenv()
 api_key = os.environ.get("OPENAI_API_KEY")
+
 with open("config.yaml", "r") as f:
     context_config = yaml.safe_load(f)
 
@@ -18,26 +15,10 @@ with open("config.yaml", "r") as f:
 user_request = context_config["user"]["request"]
 assistant_context = context_config["assistant"]["context"]
 
-# Define agent details
-agent_details = [
-    {
-        "agent_context": f"You are a accountant . {assistant_context}",
-        "agent_name": "Davide",
-    },
-    {
-        "agent_context": f"You are a economist. {assistant_context}",
-        "agent_name": "Kyle",
-    },
-    {
-        "agent_context": f"You are a investor. {assistant_context}",
-        "agent_name": "Natalia",
-    },
-    {
-        "agent_context": f"You are a marketing expert. {assistant_context} ",
-        "agent_name": "Steven",
-    },
-]
+# Suggest agent details using the function
+agent_details = suggest_agents(user_request, api_key, assistant_context)
 
+print(agent_details)
 # Create agents
 agents = create_agents(api_key, agent_details)
 
--- src/utils/agent_utils.py
+++ src/utils/agent_utils.py
@@ -1,5 +1,8 @@
 from typing import List, Dict, Tuple, Optional
 from src.agents.agent import Agent
+import random
+import openai
+import re
 
 
 def create_agents(
@@ -24,3 +27,69 @@ def create_agents(
         )
         for detail in agent_details
     }
+
+
+def suggest_agents(
+    prompt: str, api_key: str, assistant_context: str, teamsize: int = 3
+) -> list:
+    """
+    Use OpenAI to analyze a given prompt and suggest relevant agent details.
+
+    Parameters:
+        prompt (str): The problem or task description.
+        api_key (str): OpenAI API key.
+        assistant_context (str): A context description to be added to each agent's context.
+
+    Returns:
+        list: A list of dictionaries containing agent details.
+    """
+
+    competency_suggester_context = f"""
+    You are an expert in analyzing problems and suggesting the competencies or expertise 
+    required to address them. Given a task or problem and a team of max size {teamsize}, list out the competencies that would be best suited 
+    to solve it. Please list the competencies in bullet points.
+
+    Expected Output:
+    * Design: the ability to create pleasing and functional UX and UI.
+    * Development: proficiency in coding and software creation.
+    * Digital Marketing: understanding of online promotional strategies.
+    * Management: leadership and organizational skills.
+    """
+
+    openai.api_key = api_key
+    payload = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": competency_suggester_context},
+            {"role": "user", "content": prompt},
+        ],
+    }
+
+    response = openai.ChatCompletion.create(**payload)
+
+    # Extract competencies from the response using regex
+    competencies = re.findall(
+        r"\*\s*(.*?)(?:\n|$)", response.choices[0].message.content
+    )
+
+    names_pool = [
+        "Davide",
+        "Kyle",
+        "Natalia",
+        "Steven",
+        "Alex",
+        "John",
+        "Sophie",
+        "Emma",
+    ]
+    random.shuffle(names_pool)
+
+    agent_details = [
+        {
+            "agent_context": f"Your skills are \n {competency}. {assistant_context}",
+            "agent_name": names_pool[:teamsize].pop(),
+        }
+        for competency in competencies
+    ]
+
+    return agent_details
